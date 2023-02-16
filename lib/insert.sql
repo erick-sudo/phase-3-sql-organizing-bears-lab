9b5bb29c8726074a53fffb4b5f88d1c6b3efeea9
Erick Ochieng <erickochieng766@gmail.com>
2023-02-16 13:20:51 +0300

    Passed Test

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, sex) VALUES
+("Mr. Chocolate", 'M'),
+("Rowdy", 'M'),
+("Tabitha", 'F'),
+("Sergeant Brown", 'F'),
+("Melissa", 'M'),
+("Grinch", 'F'),
+("Wendy", 'M'),
+(NULL, 'F');
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,24 @@
 def selects_all_female_bears_return_name_and_age
   "Write your SQL query here"
+  "select bears.name, bears.age from bears where sex='F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "Write your SQL query here"
+  "select bears.name from bears order by bears.name"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
+  "select bears.name, bears.age from bears where bears.alive=1 order by bears.age asc"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  "select bears.name, bears.age from bears order by bears.age desc limit 1"
 end
 
 def select_youngest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  "select bears.name, bears.age from bears order by bears.age asc limit 1"
 end
